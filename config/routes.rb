d6d66a692334c362b1bc538b4460aa5c7f3a4cda
Omer A <omerzahidali@gmail.com>
2023-02-06 10:28:37 +0300

    Added mailer and mail views

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'user/new'
   get 'user/create'
   resources :posts, only: [:new, :create, :index]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
